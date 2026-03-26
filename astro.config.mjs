11981b13db1057170608244bd2bd1e8933bbc54f
Toshi <toshi.python764@gmail.com>
2026-03-26 21:41:06 +0900

    URL設定の修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://<YOUR_GITHUB_USERNAME>.github.io',
-  base: '/<YOUR_REPOSITORY_NAME>',
+  site: 'https://toshipython764-cmyk.github.io',
+  base: '/portfolio',
 });
